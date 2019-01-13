1c13410592bbac711a94303bf0302c87f1b97c68
Denis Irz <denirz@gmail.com>
2019-01-13 22:24:41 +0300

    добавито много всего вmashinegame

--- test/jasmine-standalone-3/spec/mashinegameSpec.js
+++ test/jasmine-standalone-3/spec/mashinegameSpec.js
@@ -1,6 +1,6 @@
 describe("Mashinegame function test ",function(){
  beforeEach(function(){
-         nlines = 25;
+        nlines = 25;
 
         $("body").append('<p id="nomoresteps"></p>');
         $("body").append('<canvas id="main" width="400" height="400"></canvas>')
@@ -11,30 +11,37 @@ describe("Mashinegame function test ",function(){
      drawgrid(canvas)
      });
     it("FirstStep",function(){
-        var a = new Quadrant(0,0);
-        a.m=2;
-        a.n=2;
+        let a = new Quadrant(0,0);
+        a.m = 2;
+        a.n = 2;
+        a.setcoords(canvas,nlines-4,1);
         listitems.addQuadrant(a);
-        a.setcoords(canvas,2,1)
+        a.setcoords(canvas,a.x+2,1);
         listitems.addQuadrant(a);
-        a.setcoords(canvas,4,1)
+        a.setcoords(canvas,a.x+2,1);
+        var  result = mashinegame(canvas,listmashineitems,listitems);
+        expect(result).toEqual(1);
+    });
+
+    it("Second Step",function(){
+    let a = new Quadrant(0,0);
+        a.m = 2;
+        a.n = 2;
+        a.setcoords(canvas,nlines-4,1);
         listitems.addQuadrant(a);
+        a.setcoords(canvas,a.x+2,1);
+        listitems.addQuadrant(a);
+        a.setcoords(canvas,a.x+2,1);
+        var  result = mashinegame(canvas,listmashineitems,listitems);
+        expect(result).toEqual(1);
+        a.m = 3;
+        a.n = 4;
         var  result = mashinegame(canvas,listmashineitems,listitems,a);
+        expect(result).toEqual(1);
+        var  result = mashinegame(canvas,listmashineitems,listitems,a);
+        expect(result).toEqual(1);
 
-        drawgrid(canvas)
 
-        // listitems.draw(canvas)
-        // a.clear(canvas)
-        // result = mashinegame(canvas,listmashineitems,listitems);
-
-        $("#nomoresteps").text("result="+result);
-        // let newm = new Quadrant(listmashineitems.items[0].n,listmashineitems.items[0].m);
-        // newm.n=listmashineitems.items[0].n;
-        // newm.m=listmashineitems.items[0].m;
-        // newm.setcoords(canvas,listmashineitems.items[0].x,listmashineitems.items[0].y);
-        // let result1 = newm.checkcorrectness(listitems);
-        // $("#nomoresteps").text("result="+result1);
-        // expect( result1).toEqual('Cross')
     })
 
 });
